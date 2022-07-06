854aa6f9ae45d4fd8e84bbcf7cab1c1035a5c5b2
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2022-07-06 10:24:26 +0100

    updated files

--- _config/config.php
+++ _config/config.php
@@ -12,5 +12,5 @@ define("SMTP_AUTH",true);
 define("SMTP_EMAIL","46806cb07faf74");
 define("SMTP_PASSWORD","6b0a25a89c1354");
 define("SMTP_PORT","2525");
-define("SMTP_FROM_EMAIL","aziz.traceur@gmail.com");
-define("SMTP_FROM_NAME","aziz");
+define("SMTP_FROM_EMAIL","chaibi1imran@gmail.com");
+define("SMTP_FROM_NAME","imrane");
